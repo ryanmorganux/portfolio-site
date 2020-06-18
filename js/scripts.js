de3c2f61dd827f4385294a8767b5a858f5d148d1
ryanmorganux <63609386+ryanmorganux@users.noreply.github.com>
2020-06-18 11:46:06 -0400

    updates made in accordance with task 2.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-console.log("Hi, Welcome to my Portfolio Site")
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+console.log('Hi, Welcome to my Portfolio Site')
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
